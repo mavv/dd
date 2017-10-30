a59dfbdaf745fd84e70f805b0288f7ebbcd86cc9
mavu <sickmav@gmail.com>
2017-10-30 21:11:51 +0200

    1up

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-// @flow
+// @flow weak
 
 import React, { Component } from 'react'
 
 import logo from './logo.svg'
 import './App.css'
 
-import BehanceItem from './components/BehanceItem'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
 
-class App extends Component {
+import Behance from './components/Behance'
+
+const styles = theme => ({
+  root: {
+    marginTop: theme.spacing.unit * 3,
+    width: '100%',
+  },
+});
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <BehanceItem/>
+      <div>
+
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography type="title" color="inherit">
+              Deedee's bar
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Behance />
       </div>
     )
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <h1 className="App-title">Welcome to React</h1>
+    //   </header>
+    //   <p className="App-intro">
+    //     To get started, edit <code>src/App.js</code> and save to reload.
+    //   </p>
+    //   <BehanceItem/>
+    // </div>
   }
 }
-
-export default App
